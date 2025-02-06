0fa943287fb21cdb6fa023d7a02a4a0009129787
Alex Javier <alexavier1989@gmail.com>
2025-02-06 11:38:19 -0500

    feat: Nested form with signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,70 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, effect, inject, signal } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormChildComponent } from './form-child/form-child.component';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+export interface ItemForm {
+  id: FormControl<number>;
+  name: FormControl<string>;
+  value: FormControl<number>;
+}
+
+export type CustomFormGroup = FormGroup<ItemForm>;
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ReactiveFormsModule, FormChildComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'reactiveSignalsForm';
+
+  // form: FormGroup< {items: FormArray<CustomFormGroup>}>; //old way
+  // constructor( private fb: FormBuilder){ //old way
+  //   this.form = fb.group({
+  //     items: fb.array<CustomFormGroup>([])
+  //   });
+  // }
+
+  fb = inject(NonNullableFormBuilder);
+
+  form: FormGroup< { items: FormArray<CustomFormGroup> }> = this.fb.group({
+    items: this.fb.array<CustomFormGroup>([])  
+  })
+
+  get items() {
+    return this.form.controls.items
+  }
+
+  // items = signal(this.form.controls.items.controls)
+  itemsChanged = toSignal(this.form.valueChanges);
+
+ totalValue = computed(() => {
+    const value = this.itemsChanged()?.items?.reduce((acc, item) => acc + (Number(item?.value) || 0) , 0);
+    return value;
+ })
+
+
+  // constructor(){
+  //   effect(() => {
+  //     this.form.controls.items.valueChanges.subscribe(() => {
+  //        this.items.set([...this.form.controls.items.controls]);
+  //     })
+  //   });
+  // }
+  
+  addItem() {
+    const itemId = this.items.length + 1;
+    const itemForm = this.fb.group<ItemForm>({
+      id: this.fb.control(itemId),
+      name: this.fb.control('', {validators: Validators.required}),
+      value: this.fb.control(0, {validators: Validators.required}),
+
+    })
+    this.form.controls.items.push(itemForm);
+  }
+ 
+
 }
---
+++ src/app/custom-input/custom-input.component.ts
@@ -0,0 +1,39 @@
+import { Component, forwardRef, input } from '@angular/core';
+import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-custom-input',
+  imports: [ReactiveFormsModule],
+  templateUrl: './custom-input.component.html',
+  styleUrl: './custom-input.component.scss',
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => CustomInputComponent),
+    multi: true
+  }]
+})
+export class CustomInputComponent implements ControlValueAccessor{
+  control = input.required<FormControl<any>>();
+
+ onTouched = () =>{};
+ onChange = (_value: any) => {};
+
+ writeValue(value: any): void {
+  if( this.control().value !== value ) {
+    this.control().setValue(value, { emitEvent: false});
+  }
+ }
+
+ registerOnChange(fn: any): void {
+  this.onChange = fn;
+ }
+
+ registerOnTouched(fn: any): void {
+  this.onTouched = fn;
+ }
+
+ setDisabledState(isDisabled: boolean): void {
+   isDisabled ? this.control().disable() : this.control().enable();
+ }
+
+}
